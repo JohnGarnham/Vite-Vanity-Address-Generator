8aa7cd4ce139d92ab8eff52bd567ecb46e82ff4c
JohnGarnham <john.e.garnham@gmail.com>
2021-10-09 01:35:05 -0600

    Add input controls to UI

--- src/App.js
+++ src/App.js
@@ -6,15 +6,22 @@ function App() {
 
   return (
     <div className="vanity-body">
-        <div className="header">
-            <h2>Vite Vanity Address Creator</h2>
-        </div>
-
-      <div className="input-buttons">
-   
-          <button type="button" className="button-column" name="Generate"></button>
-          <button type="button" className="button-column" name="Reset"></button>
-   
+      <div className="header">
+        <h2>Vite Vanity Address Creator</h2>
+      </div>
+      <div className="input-text-row">
+        Prefix: <input type="text" className="text-input" id="prefix" name="prefix" />
+      </div>
+      <div className="input-text-row">
+        Suffix: <input type="text" className="text-input" id="suffix" name="suffix" />
+      </div>
+      <div className="input-button-row">
+        <button type="button" className="input-button" name="Generate">
+          Generate
+        </button>
+        <button type="button" className="input-button" name="Reset">
+          Reset
+        </button>
       </div>
     </div>
   );
