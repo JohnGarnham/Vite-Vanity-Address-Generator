31d69b0664e0644366cd8515d0cc9de50116e07a
JohnGarnham <john.e.garnham@gmail.com>
2021-10-13 03:18:23 -0600

    Change favicon, title

--- src/App.js
+++ src/App.js
@@ -6,11 +6,6 @@ const DEFAULT_ITERATIONS = 1000;
 
 export default class VanityAddressForm extends React.Component {
 
-  componentDidMount() {
-    // Set title of page
-    document.title = "Vite Vanity Address Generator";
-  }
-
   constructor(props) {
     super(props);
 
