47a2bb467078dd3cb09177445cb2785ce8e914f8
JohnGarnham <john.e.garnham@gmail.com>
2021-10-18 00:25:37 -0600

    Updates

---
+++ worker copy.js
@@ -0,0 +1,29 @@
+import { searchAddresses } from "./findVanityAddress";
+
+export function doSearch() {
+    const use_prefix = true;
+    const use_suffix = false;
+    const prefix = "aa";
+    const suffix = "";
+    const count = 500;
+
+
+}
+
+onmessage = (e) => {
+
+    console.log("In Webworker");
+    
+    const search = e.data;
+
+    const use_prefix = search.use_prefix;
+    const prefix = search.prefix;
+    const use_suffix = search.use_suffix;
+    const suffix = search.suffix;
+    const count = search.count;
+
+    // Debug log
+    console.log("In searchAddresses(${use_prefix},${prefix},${use_suffix},${suffix},${count})");*/
+    // Grab search parameters
+
+  };
